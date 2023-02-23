4002a9f2a5db0eb5fdd5b52739c1112d9931c1fc
bbrown2 <b.brown2@ymail.com>
2023-02-23 12:18:04 -0500

    Revert "change navbar logo and update footer"
    This reverts commit 9271f9137c8ff7ee5301047de81afad1eac29611.

--- src/components/Header/header.styles.js
+++ src/components/Header/header.styles.js
@@ -1,19 +1,20 @@
-import styled from "styled-components";
-import { Container, device } from "../globalStyles";
-import { Link } from "react-router-dom";
+import styled from 'styled-components';
+import { Container, device } from '../globalStyles';
+import { Link } from 'react-router-dom';
 
 export const HeaderContainer = styled(Container)`
-  background-color: var(--colorWhite);
-  height: 100px;
-  width: 100%;
-  display: flex;
-  justify-content: center;
-  align-items: center;
+    background-color: var(--colorWhite);
+	height: 100px;
+    width: 100%;
+	display: flex;
+	justify-content: center;
+	align-items: center;
 
-  //--------> MEDIA QUERIES <--------
-  @media ${device.tablet} {
-    justify-content: space-between;
-  }
+	//--------> MEDIA QUERIES <--------
+	@media ${device.tablet} {
+		justify-content: space-between;
+	}
+
 `;
 
 //===============================================================
@@ -21,123 +22,131 @@ export const HeaderContainer = styled(Container)`
 //===============================================================
 
 export const Backdrop = styled.div`
-  background-color: rgba(0, 0, 0, 0.8);
-  position: fixed;
-  width: 100vw;
-  height: 100vh;
-  top: 0;
-  z-index: 2;
-  display: ${({ slide }) => (slide ? "block" : "none")};
+	background-color: rgba(0, 0, 0, 0.8);
+	position: fixed;
+	width: 100vw;
+	height: 100vh;
+	top: 0;
+	z-index: 2;
+	display: ${({ slide }) => (slide ? 'block' : 'none')};
 
-  //--------> MEDIA QUERIES <--------
-  @media ${device.tablet} {
-    display: none;
-  }
+	//--------> MEDIA QUERIES <--------
+	@media ${device.tablet} {
+		display: none;
+	}
 `;
 
 export const SidebarContainer = styled.div`
-  background-color: var(--colorWhite);
-  height: 100vh;
-  width: 260px;
-  position: fixed;
-  top: 0;
-  left: ${({ slide }) => (slide ? 0 : "-1000px")};
-  z-index: 2;
-  transition: all 0.3s ease-in-out;
+	background-color: var(--colorWhite);
+	height: 100vh;
+	width: 260px;
+	position: fixed;
+	top: 0;
+	left: ${({ slide }) => (slide ? 0 : '-1000px')};
+	z-index: 2;
+	transition: all .3s ease-in-out;
 
-  //--------> MEDIA QUERIES <--------
-  @media ${device.tablet} {
-    display: none;
-  }
+	//--------> MEDIA QUERIES <--------
+	@media ${device.tablet} {
+		display: none;
+	}
 `;
 
 export const SideNavUl = styled.ul`
-  width: 100%;
-  height: 200px;
-  display: flex;
-  flex-direction: column;
-  justify-content: space-around;
-  align-items: center;
-  margin-top: 100px;
-  font-size: var(--fontMd);
-  font-weight: var(--fontBold);
+	width: 100%;
+	height: 200px;
+	display: flex;
+	flex-direction: column;
+	justify-content: space-around;
+	align-items: center;
+	margin-top: 100px;
+	font-size: var(--fontMd);
+	font-weight: var(--fontBold);
 `;
 
-export const SideNavLi = styled.li`
-  list-style: none;
-`;
+export const SideNavLi = styled.li`list-style: none;`;
 
 export const SideItemLink = styled(Link)`
-  text-decoration: none;
-  color: var(--colorBlack);
+	text-decoration: none;
+	color: var(--colorBlack);
 
-  &:hover {
-    color: var(--colorPurple);
-  }
+	&:hover {
+		color: var(--colorPurple);
+	}
 `;
 
 export const Icon = styled.div`
-  position: absolute;
-  height: 50px;
-  width: 50px;
-  top: 0;
-  left: 0;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  font-size: var(--fontMd);
-  z-index: 1;
-  cursor: pointer;
-  color: var(--colorBlack);
+	position: absolute;
+	height: 50px;
+	width: 50px;
+	top: 0;
+	left: 0;
+	display: flex;
+	justify-content: center;
+	align-items: center;
+	font-size: var(--fontMd);
+	z-index: 1;
+	cursor: pointer;
+	color: var(--colorBlack);
 
-  //--------> MEDIA QUERIES <--------
-  @media ${device.tablet} {
-    display: none;
-  }
+	//--------> MEDIA QUERIES <--------
+	@media ${device.tablet} {
+		display: none;
+	}
 `;
 
 //===============================================================
 //Navbar
 //===============================================================
 export const LogoContainer = styled.div`
-  display: flex;
-  align-items: center;
+	display: flex;
+	align-items: center;
+	img {
+		height: 50px;
+		width: 100%;
+	}
 
-  //--------> MEDIA QUERIES <--------
-  @media ${device.mobileL} {
-    //
-  }
+	//--------> MEDIA QUERIES <--------
+	@media ${device.mobileL} {
+		img {
+			height: 60px;
+		}
+	}
 
-  @media ${device.tablet} {
-    justify-content: flex-start;
-  }
+	@media ${device.tablet} {
+		justify-content: flex-start;
+
+		img {
+			height: 70px;
+		}
+	}
 `;
 
 export const NavUl = styled.ul`
-  display: none;
+	display: none;
 
-  //--------> MEDIA QUERIES <--------
-  @media ${device.tablet} {
-    min-width: 400px;
-    display: flex;
-    justify-content: space-evenly;
-  }
+	//--------> MEDIA QUERIES <--------
+	@media ${device.tablet} {
+		min-width: 400px;
+		display: flex;
+		justify-content: space-evenly;
+	}
 `;
 
 export const NavLi = styled.li`
-  font-size: var(--fontSm);
-  font-weight: var(--fontBold);
-  list-style: none;
+	font-size: var(--fontSm);
+	font-weight: var(--fontBold);
+	list-style: none;
 
-  @media ${device.laptopL} {
-    font-size: 18px;
-  }
+	@media ${device.laptopL} {
+		font-size: 18px;
+	}
 `;
 export const ItemLink = styled(Link)`
-  color: var(--colorBlack);
-  text-decoration: none;
+	color: var(--colorBlack);
+	text-decoration: none;
 
-  &:hover {
-    color: var(--colorPurple);
-  }
+	&:hover {
+		color: var(--colorPurple);
+	}
 `;
